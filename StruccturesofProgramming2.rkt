7e4094668e072d392b36ac771aab6b610479bc7e
Raymond <raymondwalcott19@gmail.com>
2017-01-23 01:59:27 -0500

    Update

---
+++ StruccturesofProgramming2.rkt
@@ -0,0 +1,17 @@
+; This function computes the reversal of of the list without nested cases and parenthesis
+(DEFINE (reversing-general L)
+        (IF(NULL? L ) ; if list is empty return back empty list;
+           
+         '();
+         (begin
+
+           
+              (reversing-general( cdr L)) ; recursively calls the other elements of the list. 
+              (display (car L)); once the call stack returns back up it outputs the values at the beginning of each return
+            )
+         )
+        )
+
+           
+
+           
