7dff14b8f9efd9f159dd57cd30780092e2836722
Raymond <raymondwalcott19@gmail.com>
2017-01-27 14:26:05 -0500

    Update16

---
+++ Structuresofprogramming16.rkt
@@ -0,0 +1,7 @@
+;4 computes the minimum of both L1 and L2
+(define (min-above-min L1 L2)
+(begin
+  
+       (if (null? L2) #f) ; if empty return false
+      ( if (EQ? ( car '(0 1) )( car '(0 1) ))  (mini2 L2)) ;  calulcate the minimum value of L2
+
